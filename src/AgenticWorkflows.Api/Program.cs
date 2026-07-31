1f4adcbbb3d5e66cd99f9e08a7b18be58a767df7
eight9080 <eight9080@gmail.com>
2026-07-31 17:16:28 +0000

    commit test

--- src/AgenticWorkflows.Api/Program.cs
+++ src/AgenticWorkflows.Api/Program.cs
@@ -45,10 +45,6 @@ workItems.MapGet("/{id:guid}", (Guid id, WorkItemService service) =>
         Results.Ok(service.Find(id)))
     .WithName("GetWorkItem");
 
-workItems.MapGet("/{id:guid}", (Guid id, WorkItemService service) =>
-    Results.Ok(service.Find(id)))
-.WithName("GetWorkItem");
-
 workItems.MapGet("/{id:guid}/notifications", (Guid id, WorkItemService service) =>
     {
         var item = service.Find(id);
